2151fadfea4bac64fc7798d60fdb0c8dd9d362ca
Stew Wilson <stew.wilson@ed.ac.uk>
2017-08-15 11:37:07 +0100

    Update to handle file:/// urls for approver file

--- server/jssapproval/approvers.py
+++ server/jssapproval/approvers.py
@@ -1,4 +1,6 @@
-import json, urllib2, base64
+import json
+import urllib2
+import base64
 
 
 class Approvers(object):
@@ -6,23 +8,32 @@ class Approvers(object):
     """
 
     def __init__(self, url, user, password):
-        request = urllib2.Request("url")
-        base64string = base64.encodestring('%s:%s' % (user, password)).replace('\n', '')
-        request.add_header("Authorization", "Basic %s" % base64string)   
-        self.approvers = json.load(result = urllib2.urlopen(request))
+        if url[:7] == "file://":
+            with open(url[7:], 'r') as f:
+                self.approvers = json.load(f)
+        else:
+            request = urllib2.Request("url")
+            base64string = base64.encodestring('%s:%s' % (user, password)).replace('\n', '')
+            request.add_header("Authorization", "Basic %s" % base64string)   
+            self.approvers = json.load(urllib2.urlopen(request))
 
     def get_approver(self, host):
-        return "g.lee@ed.ac.uk"
         
         if host[-8:] == 'ed.ac.uk':
             area = host.split('.')[-4].upper()
         elif '-' in host:
             area = host.split('-')[0].upper()
         else:
-            raise ValueError("Machine name has no area identifier")
+            #raise ValueError("Machine name has no area identifier")
+            area = "No area identifier"
 
         try:
-            return self.approvers[area]
+            approver = self.approvers[area]
         except KeyError, e:
             # No approver defined; kick it to Helpline
-            return 'is.helpline@ed.ac.uk'
+            approver = 'is.helpline@ed.ac.uk'
+        
+        # return approver
+        print area + ":\t" + approver
+
+        return "g.lee@ed.ac.uk"
